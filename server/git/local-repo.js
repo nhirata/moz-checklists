4e068a52f116404e20683873a1c96beddedeae8b
Johan Lorenzo <jlorenzo@mozilla.com>
2015-03-17 16:46:00 +0100

    Change git library to nodegit
    This fixes the race conditions while accessing multiple files

--- server/git/local-repo.js
+++ server/git/local-repo.js
@@ -2,69 +2,76 @@
 
 var fs = require('fs');
 var Promise = require('promise');
+var NodeGit = require("nodegit");
 
 var localFolder = 'var/git';
-var git = require('simple-git')(localFolder)
-
 var remoteRepoUrl = 'http://localhost:10080/johan/moztrap-replacement-repo.git';
-
 var _branch = 'master';
+var _repo = null;
 
 var _initializeRepo = function() {
   return new Promise(function(fulfill, reject) {
     fs.exists(localFolder, function(exists) {
       if (exists) {
-        fulfill();
+        NodeGit.Repository.open(localFolder).then(function (repo) {
+          _repo = repo;
+        });
       } else {
-        git.clone(clone(remoteRepoUrl, localFolder)).then(fulfill);
+        NodeGit.Clone.clone(remoteRepoUrl, localFolder, null).then(function(repo) {
+          _repo = repo;
+        })
       }
     });
   })
 }
 
+var _getFileContentAtRevision = function(path, revision) {
+  return _repo.getCommit(revision)
+    .then(function(commit) {
+      return commit.getEntry(path);
+    })
+    .then(function(entry){
+      return entry.getBlob().then(function(blob) {
+        return String(blob);
+      });
+    })
+}
+
 var self = module.exports = {
 
   update: function() {
-    return _initializeRepo()
-    .then(function() {
-      return git.pull();
-    })
+      _repo.fetchAll({
+        credentials: function(url, userName) {
+          return NodeGit.Cred.sshKeyFromAgent(userName);
+        },
+        certificateCheck: function() {
+          return 1;
+        }
+      })
+      // Now that we're finished fetching, go ahead and merge our local branch
+      // with the new one
+      .then(function() {
+        return repository.mergeBranches("master", "origin/master");
+      })
   },
 
   getCurrentBranch: function() {
     return _branch;
   },
 
-  getFileContent: function(path) {
-    return new Promise(function(fulfill, reject) {
-      return git.checkout(self.getCurrentBranch())
-              .then(self.update)
-              .then(function() {
-                fs.readFile(localFolder + '/' + path, { encoding: 'utf8' }, function(err, data) {
-                  fulfill(data);
-                })
-              })
-    })
-  },
-
-  getFileContentAtRevision: function(path, revision) {
-    return new Promise(function(fulfill, reject) {
-      return git.checkout(revision)
-              .then(self.update)
-              .then(function() {
-                fs.readFile(localFolder + '/' + path, { encoding: 'utf8' }, function(err, data) {
-                  fulfill(data);
-                })
-              })
-    })
+  getFileContent: function(path, revision) {
+    if (typeof revision === 'undefined') {
+      return self.getLatestCommitSha1().then(function(head) {
+        return _getFileContentAtRevision(path, head);
+      })
+    } else {
+      return _getFileContentAtRevision(path, revision);
+    }
   },
 
   getLatestCommitSha1: function() {
-    return new Promise(function(fulfill, reject) {
-      // latest doesn't return the latest commit
-      return git.log(function(err, log) {
-        fulfill(log.all[0].hash);
-      })
-    })
+    return NodeGit.Reference.nameToId(_repo, "HEAD");;
   }
 };
+
+_initializeRepo();
